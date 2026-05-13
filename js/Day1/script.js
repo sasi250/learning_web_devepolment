5caf40ac7b3e4185c1b6cd5feb377ba4f1d04138
nallurisasipriyanka <118510647+nallurisasipriyanka@users.noreply.github.com>
2026-05-13 11:22:51 +0530

    js

---
+++ js/Day1/script.js
@@ -0,0 +1,22 @@
+console.log("hello")
+var num1 =10;
+var num2 =20;
+if (num1 > num2){
+    console.log(num1+"is greater than"+num2);
+
+}else{
+    console.log(num2+"is greater than"+num1)
+}
+
+var num = 10;
+if (num%2==0){
+    console.log("even")
+}else{
+    console.log("not")
+}
+ var num =2024 
+ if (num%4==0 & num%100!=0 | num%400==0){
+    console.log("leap year")
+ }else{
+    console.log("not")
+ }
