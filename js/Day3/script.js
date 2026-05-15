a862f796c935ad68c099d804f55b36b7299419b8
nallurisasipriyanka <118510647+nallurisasipriyanka@users.noreply.github.com>
2026-05-15 11:25:47 +0530

    js

---
+++ js/Day3/script.js
@@ -0,0 +1,66 @@
+// let num1 = 10;
+// let num2 = 20;
+// var op = "*"
+// switch (op) {
+//     case '+':
+//         console.log(num1+num2)
+//         break;
+//     case '-':
+//         console.log(num1-num2)
+//         break;
+//     case '*':
+//         console.log(num1*num2)
+//         break
+//     default:
+//         console.log("Invalid input")
+//         break;
+// }
+
+
+// Function
+
+// console.log(myFun(1, 1));
+
+// function myFun(num1, num2){
+//     return num1 + num2
+// }
+
+// function myFun(num1, num2){
+//     return num1-num2
+// }
+
+// function myFun(num1, num2){
+//     return num1 + num2
+// }
+// console.log(myFun(1, 1));
+
+// function myFun(num1, num2){
+//     return num1-num2
+// }
+// function myFun(num1, num2){
+//     return num1 + num2
+// }
+
+// function myFun(num1, num2){
+//     return num1-num2
+// }
+// console.log(myFun(1, 1));
+
+
+// 2.Function Expression
+// const fun = function(num1,num2){
+//     return num1+num2
+// }
+// console.log(fun(1,1));
+
+
+//3.Arrow Function
+// const fun =(num1,num2)=>{
+//     return num1+num2
+// }
+// console.log(fun(1,1));
+
+//4.Anonymous Function
+// const fun = (num1,num2) => num1+num2
+
+// console.log(fun(1,1));
